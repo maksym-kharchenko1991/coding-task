7ba32cb519919c3649a854e598ee45ce5efd0500
Максим Харченко <makskharchenko1991@gmail.com>
2023-12-21 17:17:11 +0200

    update date, add date formatting, limits for articles

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -2,40 +2,64 @@
 window.LATEST_ARTICLES =[
     {
         "title": "Find new ways to travel north",
-        "publishDate": "2023-06-03T13:51:50.417Z",
+        "publishDate": "2023-12-20T13:51:50.417Z",
         "category": "news",
         "url": "/articles/4738.html"
     },
     {
-        "title": "When will it become possible to use timetravel in order to fix your earlier mistakes?",
-        "publishDate": "2023-06-01T12:18:10.317Z",
+        "title": "In-depth travel guide for Tanzania",
+        "publishDate": "2023-12-16T11:12:43.003Z",
         "category": "essay",
-        "url": "/articles/7256.html"
+        "url": "/articles/2594.html"
     },
     {
         "title": "10 ways to write better text",
-        "publishDate": "2023-06-03T09:00:32.200Z",
+        "publishDate": "2023-12-17T09:00:32.200Z",
         "category": "news",
         "url": "/articles/7247.html"
     },
     {
         "title": "Announcement: we have a new website category",
-        "publishDate": "2023-05-30T17:12:13.102Z",
+        "publishDate": "2023-12-17T17:12:13.102Z",
         "category": "news",
         "url": "/articles/1749.html"
     },
     {
         "title": "Weekly news",
-        "publishDate": "2023-05-29T00:23:15.276Z",
+        "publishDate": "2023-12-18T00:23:15.276Z",
         "category": "news",
         "url": "/articles/1538.html"
     },
     {
-        "title": "In-depth travel guide for Tanzania",
-        "publishDate": "2023-05-31T11:12:43.003Z",
+        "title": "News News News",
+        "publishDate": "2023-12-20T09:00:32.200Z",
+        "category": "news",
+        "url": "/articles/7247.html"
+    },
+    {
+        "title": "Outdated",
+        "publishDate": "2023-12-10T09:00:32.200Z",
+        "category": "essay",
+        "url": "/articles/7232.html"
+    },
+    {
+        "title": "When will it become possible to use time travel in order to fix your earlier mistakes?",
+        "publishDate": "2023-12-20T12:18:10.317Z",
+        "category": "essay",
+        "url": "/articles/7256.html"
+    },
+    {
+        "title": "Test article",
+        "publishDate": "2023-12-20T12:18:10.317Z",
+        "category": "test",
+        "url": "/articles/7156.html"
+    },
+    {
+        "title": "Essay Essay Essay",
+        "publishDate": "2023-12-19T11:12:43.003Z",
         "category": "essay",
         "url": "/articles/2594.html"
-    }
+    },
 
 ];
 
@@ -49,12 +73,27 @@ export default {
     },
     computed: {
         filteredArticles() {
+            const currentDate = new Date();
+            currentDate.setDate(currentDate.getDate() - 7); 
+
             const filtered = this.articles.filter(item => {
-                return (this.showNews && item.category === 'news') ||
-                    (this.showEssay && item.category === 'essay')
+                const itemDate = new Date(item.publishDate);
+                return ((this.showNews && item.category === 'news') ||
+                    (this.showEssay && item.category === 'essay')) &&
+                    (itemDate >= currentDate);
             })
 
             return filtered.sort((a, b) => new Date(b.publishDate) - new Date(a.publishDate));
+        }
+    },
+    methods: {
+        formatDate(date) {
+            const options = { 
+                month: 'long', 
+                day: 'numeric', 
+            };
+
+            return new Date(date).toLocaleDateString('en-US', options);
         }
     }
 }
@@ -68,8 +107,8 @@ export default {
             <label><input type="checkbox" v-model="showNews">News</label>
             <label><input type="checkbox" v-model="showEssay">Essay</label>
             <ul>
-                <li v-for="(article) in filteredArticles" :key="article.title">
-                    <a :href="article.url">{{ article.title }} {{ article.publishDate }}</a>
+                <li v-for="(article) in filteredArticles.slice(0, 5)" :key="article.title">
+                    <a :href="article.url">{{ article.title }} {{ formatDate(article.publishDate) }}</a>
                 </li>
             </ul>
         </div>
